93fb233398a5f696c986a13f78cc19c74897c6b3
LimKyungHyuk <yorsild@naver.com>
2018-04-17 19:44:30 +0900

    list

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,8 +7,16 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  items = [
+    {"id":1, "name":"첫번째 Item"},
+    {"id":2, "name":"두번째 Item"},
+    {"id":3, "name":"세번째 Item"}
+  ];
 
+  userName : any;
+
+  constructor(public navCtrl: NavController) {
+    this.userName = "아이오닉";
   }
 
 }
